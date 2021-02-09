0abdcfae0557f3c7ff71911813163c5366c96523
hcaouette <huntercaouette@gmail.com>
2021-02-08 19:52:02 -0500

    changed python approach to use pd merge instead of numpy map

--- cars_matplot.py
+++ cars_matplot.py
@@ -5,20 +5,13 @@ import pandas as pd
 def main():
     # data load
     cars = pd.read_csv('cars-sample.csv')
-    # manufacturer and color association
-    color_map = {'ford':0,'toyota':1,'bmw':2,'honda':3,'mercedes':4}
-    color = np.array(['#6e750e','#c20078','#e50000','#15b01a','#75bbfd'])
-    cars['color_map']=cars.Manufacturer.map(color_map,na_action='ignore')
-    # print(cars.head())
-    col_map = cars.color_map.to_numpy()
-    # print(col_map)
+    c_map = pd.DataFrame({'Manufacturer':cars['Manufacturer'].unique(),'color':['#6e750e','#c20078','#e50000','#15b01a','#75bbfd']})
+    cars = cars.merge(c_map, on='Manufacturer', how='left')
 
-    #matplot part
     fig, ax = plt.subplots()
-    lab = cars['Manufacturer'].unique() #'ford' 'toyota' 'bmw' 'honda' 'mercedes'
     x = cars['Weight']
     y = cars['MPG']
-    ax.scatter(x, y, c = color[col_map], s = cars['Weight'], alpha = 0.5,)# label = lab )
+    ax.scatter(x, y, c = cars['color'], s = cars['Weight'], alpha = 0.5,)# label = cars['Manufacturer'].unique() )
     ax.legend()
     ax.grid(True)
     plt.show()
